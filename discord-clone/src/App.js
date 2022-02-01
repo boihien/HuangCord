58632e13a4bc72711b7fe1fee7e2e559092e0de4
Boi-Hien Huang <bhuan019@ucr.edu>
2022-01-31 19:42:11 -0800

    added login ... firebase wip

--- discord-clone/src/App.js
+++ discord-clone/src/App.js
@@ -2,11 +2,22 @@ import React from "react";
 import "./App.css";
 import Chat from "./Chat";
 import Sidebar from "./Sidebar";
+import {useSelector} from "react-redux";
+import {selectUser} from './features/userSlice';
+import Login from "./Login";
+
 function App() {    
+    const user=useSelector(selectUser);
      return (
       <div className="app">
-        <Sidebar />
-        <Chat />
+        {user ? (
+          <>
+            <Sidebar />
+            <Chat />
+          </>
+        ) : (
+          <Login/>
+        )}
       </div>
   );
 }
